c3f26bcf6e476eaedd21b7859ee8f16a21f3ffaa
JaviThomas <dog2785@gmail.com>
2023-05-10 20:55:02 -0400

    adding item class

---
+++ randomCode/JavisBizarreAdventure/itemclass.js
@@ -0,0 +1,10 @@
+class Item {
+    constructor(name, hpbuff, attbuff, apbuff, uses) {
+      this.name = name;
+      this.hpbuff = hpbuff;
+      this.attbuff = attbuff;
+      this.apbuff = apbuff;
+      this.uses = uses;
+    }
+  }
+export default Item;
