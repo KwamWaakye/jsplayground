62f45795a37d49e45e318b66a7cebf937355febb
KwamWaakye <BoakyeK11@outlook.com>
2023-06-17 19:50:20 -0400

    new prompt bruh

--- randomCode/JavisBizarreAdventure/levels/levelone.js
+++ randomCode/JavisBizarreAdventure/levels/levelone.js
@@ -1,22 +1,48 @@
 // imported libraries 
-const prompt = require("prompt-sync")();
+import gradient from "gradient-string";
+import prompt from "prompt-sync";
+import chalk from 'chalk';
+import select, { Separator } from '@inquirer/select'; 
+// INITIALIZING PROMPT 
+const userInput = prompt();
+
+//CREATING EASIER CONSOLE.LOG AS LOG
+const log = console.log;
 // level information that we will give to the main game runner
 var leveldata = {}; 
 var input = "";
-const Sequence1 = () => {
-    console.log("What is your name?")
-    input = prompt(">>> ");
+export const Sequence1 = async () => {
+    log(chalk.bold.blue("What is your name?"))
+    input = userInput(">>> ");
 
-    console.log(`Oh, so your name is ${input}`);
+    log(`Oh, so your name is ${input}`);
     leveldata["username"] = input;
-    console.log("What did you do to get put in here? \n\t 1:I attempted to kill my bot lane for inting\n\t 2:I got into a bar fight with Kwame because he said my Garen lackin'\n");
-    input = prompt(">>> ")
-    if (input == 1){
+    const answer = await select({
+        message: "What did you do to get put in here?",
+        choices: [
+          {
+            value: 1,
+            name: "I attempted to kill my bot lane for inting",
+            description: 'attempted murder was the case they gave me',
+          },
+          {
+            value: 2,
+            name: 'I got into a bar fight with Kwame because he said my Garen SUCKS',
+            description: 'ackshually my Garen goated',
+          },
+        ],
+      });
+// log("What did you do to get put in here? \n\t 1:I attempted to kill my bot lane for inting\n\t 2:I got into a bar fight with Kwame because he said my Garen lackin'\n");
+// input = userInput(">>> ")
+    if (answer == 1){
         Sequence1_1();
     }
 };
 const Sequence1_1 = () => {
-    input = prompt("Attempted, what? Are you alright in the head?");
-
+    log("Attempted, what? Are you alright in the head?");
+    log(gradient("red", "yellow")("You have received MIDLANERS ANGUISH"))
 };
-Sequence1();
+const Sequence1_2 = () => {
+    log("You fought him over what? Perhaps you need some grass to touch!");
+    log
+};
